699965f77e2c710a92cf3f78f8d8a6ccffc4000d
Ann Susan <anns@qburst.com>
2020-09-30 23:40:52 +0530

    Responsive login, register pages without dynamic page title load

---
+++ js/login.js
@@ -0,0 +1,12 @@
+myStorage = window.localStorage;
+
+
+// myStorage.setItem("name", "[{1}, {2:a}, {3}]" );
+// console.log(myStorage.getItem("name"));
+
+// userList, 
+// userinfo = {fname: , lname: , dOB: , gender: , pswd: }
+// JSON.parse(userinfo)
+
+// multiple users?
+// FormData?
